ef7d7e86e7631cae5ef27d40390d7499159f0c36
Anthony Delosa <adelosa@gmail.com>
2017-10-21 14:50:50 +1000

    Add database support

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,33 @@
+import os
+from sqlalchemy import String, Integer, Column, create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
 from flask import Flask
-application = Flask(__name__)
 
-@application.route("/")
+Base = declarative_base()
+
+
+class HelloTable(Base):
+    __tablename__ = 'hello_table'
+
+    id = Column(Integer, primary_key=True)
+    greeting = Column(String, primary_key=True)
+
+
+app = Flask(__name__)
+db_url = 'postgresql://{user}:{pwd}@{host}/{db}'.format(host=os.environ('POSTGRESQL_SERVICE_HOST'), user=os.environ('DB_USER'), pwd=os.environ('DB_PASS'), db='hellodb')
+app.engine = create_engine(os.environ.get(db_url), echo=True)
+Base.metadata.create_all(bind=app.engine)
+
+app.db_session = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=app.engine))
+Base.query = app.db_session.query_property()
+
+
+@app.route("/")
 def hello():
     return "Hello World!"
 
+
 if __name__ == "__main__":
-    application.run()
+    app.run()
